1fb35ecf6386a6d4ea9985d20762fe7c3fd14dff
ericmahare <erikmahare@gmail.com>
2022-07-18 19:03:39 +0300

    Add private method to the class

--- person.rb
+++ person.rb
@@ -1,19 +1,24 @@
 class Person
+  attr_reader :id
   attr_accessor :name, :age
-  attr_reader :id,
-              @id
 
   def initialize(age, name: 'Unknown', parent_permission: true)
+    @id =  @id = Random.rand(1...100)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
+  private
   def of_age?
-    age >= 18
+    @age >= 18
   end
 
+  public
   def can_use_services?
     @age >= 18 || @parent_permission
   end
 end
+
+person = Person.new(34)
+p person.can_use_services?
