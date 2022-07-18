9647949e9c3546769c17c5691ad87c61fa5f3671
ericmahare <erikmahare@gmail.com>
2022-07-18 18:01:07 +0300

    Include student class

---
+++ student.rb
@@ -0,0 +1,15 @@
+require_relative('./person')
+class Student < Person
+  
+  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+    super(age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end 
+end 
+
+tch = Student.new(30, 'CS')
+p tch.age
